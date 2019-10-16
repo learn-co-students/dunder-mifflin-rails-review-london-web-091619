00b53da977d6464a3a6ce456ff0e91ac298daa25
Tim Golding <tgolding55@hotmail.com>
2019-10-16 15:28:58 +0100

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,12 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+   def self.sort_dogs
+    all.order('employee_count DESC')
+   end
+
+
+   def self.set_employee_counts
+    all.each { |dog|  dog.update(employee_count: dog.employees.length) }
+   end
 end
