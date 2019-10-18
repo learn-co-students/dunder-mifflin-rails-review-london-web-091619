56c2a0893a583b51ddc336ecbccccfcb19ad2bb3
MARCO MCNULTY <Marco@MARCOs-MacBook-Pro-2.local>
2019-10-18 11:33:47 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
 end
